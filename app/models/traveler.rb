dc3bad4a6336a710e9724b35e8194afafb4b3a74
William Luck <luck.william.h@gmail.com>
2022-10-13 15:55:43 -0300

    add methods for unique countries, count of stays

--- app/models/traveler.rb
+++ app/models/traveler.rb
@@ -2,6 +2,15 @@ class Traveler < ActiveRecord::Base
     has_many :visits
     has_many :countries, through: :visits
 
-    
+    # Don't need nationality name
+    # Don't need passport number
+
+    def total_countries_visited
+        self.countries.uniq.count
+    end
+
+    def count_of_stays
+        self.visits.count
+    end
 
 end
