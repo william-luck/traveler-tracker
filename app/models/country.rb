215e574804b6de98e445c86f0e250fd214b147e4
William Luck <luck.william.h@gmail.com>
2022-10-13 14:54:57 -0300

    define method for traveler count in each country

--- app/models/country.rb
+++ app/models/country.rb
@@ -19,20 +19,23 @@ class Country < ActiveRecord::Base
                 travelers_in_country << traveler
             end
         end
-        travelers_in_country
+        travelers_in_country.uniq
+    end
+
+    def count_of_travelers
+        travelers_currently_in_country.count
+    end
+
+    def self.travelers_in_country_array
+        number_array = []
+        Country.all.each do |country|
+            number_array << country.count_of_travelers
+        end
+        number_array
     end
 
     # def first_one
     #     self.all.first
     # end
 
-    # Need a method to access all the travlers of a country? 
-    # Wouldn't this just be Countries.travelers?
-    # Not exactly... Has to be find matching country from the list, and then select that country and all of its travelers
-
-    # So we use the class / instance method to return an array, then conver that to JSON
-
-    # We could either do this from country.travelers (need a get a way to get that country first)
-
-    
 end
