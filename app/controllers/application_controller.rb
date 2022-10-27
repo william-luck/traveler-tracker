95cbb25be6bbf1fdd4e3e5dd0d505ae60427b5f1
William Luck <luck.william.h@gmail.com>
2022-10-27 10:09:47 -0300

    refactor controller routes for RESTful

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,67 +3,46 @@ require 'pry'
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Good
   get '/countries' do
     countries = Country.all
     countries.to_json(include: :travelers_currently_in_country)
   end
 
-  # Good
   get '/countries/:id' do
     country = Country.find(params[:id])
     country.to_json(include: { travelers_currently_in_country: {include: :visits_of_current_country }})
   end
 
-  # # Good
-  # get '/visits/:id' do
-  #   selected_traveler = Traveler.find(params[:id])
-  #   selected_traveler.visits.to_json
-  # end
-
-  # Good
   get '/traveler/:id' do
-    traveler = Traveler.find(params[:id])
+    if params[:id].length < 9
+      traveler = Traveler.find(params[:id])
+    else
+      traveler = Traveler.find_by(passport_number: params[:id])
+    end
     statistics = traveler.traveler_statistics
     statistics.to_json
   end
 
-  # Good
   patch '/traveler/:id' do
     traveler = Traveler.find(params[:id])
     traveler.edit_name(params).to_json
     traveler.edit_passport_number(params).to_json
   end
 
-  # delete traveler/:id
-  delete '/traveler_delete/:id' do
+  delete '/traveler/:id' do
     traveler = Traveler.find(params[:id])
     traveler.delete_traveler.to_json
-
   end
 
-  # post traveler
-
-  post '/add_traveler' do
-
+  post '/traveler' do
     country = Country.find_by(country_name: params[:nationality])
     Traveler.add_traveler(params, country.id).to_json
-
   end
 
-  # post visit
-
-  post '/add_visit' do
+  post '/visit' do
     country = Country.find_by(country_name: params[:country_name])
     traveler = Traveler.find_by(passport_number: params[:passport_number])
     Visit.add_visit(params, country.id, traveler.id).to_json
-
-  end
-
-  # Don't really need this one.... traveler statistics
-  get '/lookup_traveler/:passport_number' do
-    traveler = Traveler.find_by(passport_number: params[:passport_number])
-    traveler.traveler_statistics.to_json
   end
 
 end
